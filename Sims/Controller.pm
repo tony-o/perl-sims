47fba9f68e57497c031bb3f548631f4dfd48b07a
Tony O'Dell <tony.odell@live.com>
2014-05-17 09:47:26 -0700

    battle buttes

--- Sims/Controller.pm
+++ Sims/Controller.pm
@@ -7,28 +7,36 @@ use Data::Dump qw(dump);
 my @sims;
 
 sub new{
-  my $class = shift;
+  my ($class) = @_;
   my $self = {
     INTERVAL => 5    
     ,QUEUE => []
-    ,SIMS => [] 
     ,SIMULATION_VARIABLES => {}
+    ,IDCACHE => {}
   };
   bless $self, $class;
   return $self;
 }
 
+sub looper {
+  my ($self) = @_;
+  foreach (@{$self->{QUEUE}}){
+    $_->($self, $self->{SIMULATION_VARIABLES});
+  }
+  my ($k,$v);
+  foreach (keys %{$self->{IDCACHE}}) {
+    $k = $_;
+    $v = $self->{IDCACHE}->{$k};
+    next if !defined $self->{IDCACHE}->{$k};
+    print "run: " . $v->{_ID}, "\n";
+    $v->{RUN}($self->{SIMULATION_VARIABLES}, 5);
+  }
+}
+
 sub go{
   my ($self,$params) = @_;
   $self->{INTERVAL} = $params->{INTERVAL} || $self->{INTERVAL};
-  $self->{TIMER} = AE::timer 0, $self->{INTERVAL}, sub{
-    foreach (@{$self->{QUEUE}}){
-      $_->($self->{SIMULATION_VARIABLES});
-    }
-    foreach (@{$self->{SIMS}}){
-      $_->{RUN}($self->{SIMULATION_VARIABLES}, 5);
-    }
-  };
+  $self->{TIMER} = AE::timer 0, $self->{INTERVAL}, sub { $self->looper };
   $self->{READY} = AE::cv;
   $self->{READY}->recv;
   return !0;
@@ -37,7 +45,21 @@ sub go{
 
 sub add_sim{
   my ($self,$sim) = @_;
-  push(@{$self->{SIMS}}, $sim);
+  die 'ID ALREADY USED' if defined $self->{IDCACHE}->{$sim->{_ID}};
+  $self->{IDCACHE}->{$sim->{_ID}} = $sim;
+  print "IDCACHE: " . $sim->{_ID} . "\n";
+  return !0;
+}
+
+sub kill_sim{
+  my ($self,$id) = @_;
+  print "del: $id\n";
+  delete $self->{IDCACHE}->{$id};
+}
+
+sub sim_bus{
+  my ($self, $id) = @_;
+  return $self->{IDCACHE}->{$id};
   return !0;
 }
 
--- test.pl
+++ test.pl
@@ -11,28 +11,66 @@ use Sims::Humanoid;
 my $controller = Sims::Controller->new();
 my @sims;
 
-foreach (1..20) {
+foreach (1..2) {
   push(@sims, Sims::Player->new($_));
 }
 
+
+my @grid;
+foreach (0..2){ push @grid, [0,0,0];};# [0,0,0, 0,0,0, 0,0,0, 0]; }
 my $simrules = sub{
   my ($self, @env) = @_;
-  say 'sim #' . $self->{_ID} . ' checking in';
+  if (!defined $self->{X}) {
+    $self->{X} = int(rand(3));
+    $self->{Y} = int(rand(3));
+  }
+  my $nextdir = int(rand(4));
+  say "ID: " . $self->{_ID};
+  # 0 = left, 1 = up, 2 = right, 3 = down
+  $grid[$self->{X}][$self->{Y}] = 0;
+  $self->{X}-- if ($nextdir == 0 && $self->{X} - 1 >= 0);
+  $self->{X}++ if ($nextdir == 2 && $self->{X} + 1 < 3);
+  $self->{Y}-- if ($nextdir == 1 && $self->{Y} - 1 >= 0);
+  $self->{Y}++ if ($nextdir == 3 && $self->{Y} + 1 < 3);
+  if ($grid[$self->{X}][$self->{Y}] != 0) {
+    my $killid = $controller->sim_bus($grid[$self->{X}][$self->{Y}]);
+    say "kill:" . $killid->{_ID};
+    $controller->kill_sim($killid->{_ID});
+    my $newsim = Sims::Player->new(scalar(@sims) + 1);
+    say "add:" . $newsim->{_ID};
+    push @sims, $newsim;
+    $controller->add_sim($newsim);
+    setupsim($newsim);
+  }
+  $grid[$self->{X}][$self->{Y}] = $self->{_ID};
+
 };
+sub setupsim {
+  my ($sim) = @_;
+  $sim->add_queue($simrules);
+}
 
 foreach (@sims) { 
-  $_->add_queue($simrules); 
+  setupsim($_);
   $controller->add_sim($_);
 }
 
 $controller->add_process(sub{
-  my $session = shift;
-#global rules
+  my ($self,$session) = @_;
+  #global rules
+  say '-';
+  foreach (@grid) {
+    foreach (@{$_}) {
+      printf "%03d ", $_;
+    }
+    print "\n";
+  }
+  say '-';
   return !0;
 });
 
 
 $controller->go({
-  INTERVAL => 1 
+  INTERVAL => 1, 
 });
 
