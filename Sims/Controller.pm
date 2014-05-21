a6d712e9aa852ad77a23b541865fe119fdfe5bc0
Tony O'Dell <tony.odell@live.com>
2014-05-20 17:29:02 -0700

    mortal kombat

--- Sims/Controller.pm
+++ Sims/Controller.pm
@@ -12,7 +12,7 @@ sub new{
     INTERVAL => 5    
     ,QUEUE => []
     ,SIMULATION_VARIABLES => {}
-    ,IDCACHE => {}
+    ,SIMS => {}
   };
   bless $self, $class;
   return $self;
@@ -24,11 +24,10 @@ sub looper {
     $_->($self, $self->{SIMULATION_VARIABLES});
   }
   my ($k,$v);
-  foreach (keys %{$self->{IDCACHE}}) {
+  foreach (keys %{$self->{SIMS}}) {
     $k = $_;
-    $v = $self->{IDCACHE}->{$k};
-    next if !defined $self->{IDCACHE}->{$k};
-    print "run: " . $v->{_ID}, "\n";
+    $v = $self->{SIMS}->{$k};
+    next if !defined $self->{SIMS}->{$k};
     $v->{RUN}($self->{SIMULATION_VARIABLES}, 5);
   }
 }
@@ -45,21 +44,19 @@ sub go{
 
 sub add_sim{
   my ($self,$sim) = @_;
-  die 'ID ALREADY USED' if defined $self->{IDCACHE}->{$sim->{_ID}};
-  $self->{IDCACHE}->{$sim->{_ID}} = $sim;
-  print "IDCACHE: " . $sim->{_ID} . "\n";
+  die 'ID ALREADY USED' if defined $self->{SIMS}->{$sim->{_ID}};
+  $self->{SIMS}->{$sim->{_ID}} = $sim;
   return !0;
 }
 
 sub kill_sim{
   my ($self,$id) = @_;
-  print "del: $id\n";
-  delete $self->{IDCACHE}->{$id};
+  delete $self->{SIMS}->{$id};
 }
 
 sub sim_bus{
   my ($self, $id) = @_;
-  return $self->{IDCACHE}->{$id};
+  return $self->{SIMS}->{$id};
   return !0;
 }
 
--- test.pl
+++ test.pl
@@ -10,37 +10,53 @@ use Sims::Humanoid;
 
 my $controller = Sims::Controller->new();
 my @sims;
+my $simcount = 90;
 
-foreach (1..2) {
+foreach (1 .. $simcount) {
   push(@sims, Sims::Player->new($_));
 }
 
 
+sub winner {
+  my ($sim1,$sim2) = @_;
+
+  return 1 if $sim1->{STR} >= $sim2->{STR};
+  return 0;
+}
+
 my @grid;
-foreach (0..2){ push @grid, [0,0,0];};# [0,0,0, 0,0,0, 0,0,0, 0]; }
+my $gridsize = 10;
+foreach (0 .. $gridsize-1){ push @grid, [((0) x $gridsize)];};
 my $simrules = sub{
   my ($self, @env) = @_;
   if (!defined $self->{X}) {
-    $self->{X} = int(rand(3));
-    $self->{Y} = int(rand(3));
+    my ($x,$y) = (int(rand($gridsize)),int(rand($gridsize)));
+    while ($grid[$x][$y] != 0) {
+      ($x,$y) = (int(rand($gridsize)),int(rand($gridsize)));
+    }
+    $self->{X} = $x;
+    $self->{Y} = $y;
+    $self->{STR} = int(rand(100));
   }
-  my $nextdir = int(rand(4));
-  say "ID: " . $self->{_ID};
-  # 0 = left, 1 = up, 2 = right, 3 = down
+  my $nextdir = int(rand(5));
+  #say "ID: " . $self->{_ID} . "\t" . $self->{STR};
+  # 0 = left, 1 = up, 2 = right, 3 = down, 4 = nothing
   $grid[$self->{X}][$self->{Y}] = 0;
   $self->{X}-- if ($nextdir == 0 && $self->{X} - 1 >= 0);
-  $self->{X}++ if ($nextdir == 2 && $self->{X} + 1 < 3);
+  $self->{X}++ if ($nextdir == 2 && $self->{X} + 1 < $gridsize);
   $self->{Y}-- if ($nextdir == 1 && $self->{Y} - 1 >= 0);
-  $self->{Y}++ if ($nextdir == 3 && $self->{Y} + 1 < 3);
+  $self->{Y}++ if ($nextdir == 3 && $self->{Y} + 1 < $gridsize);
   if ($grid[$self->{X}][$self->{Y}] != 0) {
-    my $killid = $controller->sim_bus($grid[$self->{X}][$self->{Y}]);
-    say "kill:" . $killid->{_ID};
-    $controller->kill_sim($killid->{_ID});
-    my $newsim = Sims::Player->new(scalar(@sims) + 1);
-    say "add:" . $newsim->{_ID};
-    push @sims, $newsim;
-    $controller->add_sim($newsim);
-    setupsim($newsim);
+    my $battleid = $controller->sim_bus($grid[$self->{X}][$self->{Y}]);
+    my $result   = winner($self, $battleid);
+    $controller->kill_sim($battleid->{_ID}) if $result == 1;
+    $controller->kill_sim($self->{_ID}) if $result == 0;
+    #my $newsim = Sims::Player->new(scalar(@sims) + 1);
+    #say "add:" . $newsim->{_ID};
+    #push @sims, $newsim;
+    #$controller->add_sim($newsim);
+    #setupsim($newsim);
+    return if $result == 0;
   }
   $grid[$self->{X}][$self->{Y}] = $self->{_ID};
 
@@ -65,7 +81,10 @@ $controller->add_process(sub{
     }
     print "\n";
   }
-  say '-';
+  $self->{POPULATION} = 1 if !defined $self->{POPULATION};
+  say '- ' . sprintf("%02d", scalar(keys %{$self->{SIMS}}) * 100 / $self->{POPULATION}) . ', ' . scalar(keys %{$self->{SIMS}});
+  $self->{POPULATION} = scalar(keys %{$self->{SIMS}});
+  $self->{READY}->send if $self->{POPULATION} == 1;
   return !0;
 });
 
